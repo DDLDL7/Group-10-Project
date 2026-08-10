c1e8ad13842de0cb891bb648cd892f25c6fdc300
Glover <edwin.glover@ashesi.edu.gh>
2026-08-10 13:04:44 +0000

    yooo

--- grid-analysis/examined data.py
+++ grid-analysis/examined data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+
 
 
 utilities = pd.read_csv('utilities.csv')
@@ -42,10 +42,7 @@ print(lines['Utility ID'].value_counts())
 connections = pd.concat([lines['Source Substation'], lines['Destination Substation']])
 print(connections.value_counts().head(10))
 
-substations['Region'].value_counts().plot(kind='bar', title='Substations by Region')
-plt.savefig('eda_regions.png')
-plt.show()
 
-connections.value_counts().head(10).plot(kind='bar', title='Top 10 Most-Connected Substations')
-plt.savefig('eda_top_substations.png')
-plt.show()
+
+
+
